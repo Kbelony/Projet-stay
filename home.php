67c6876537e54a598873f4d21643eef04adb6600
Océane Raubitschek <oceane@mbp-de-oceane.home>
2021-11-18 10:34:50 +0100

    read

--- home.php
+++ home.php
@@ -15,13 +15,61 @@ if (isset($_SESSION['id']) && isset($_SESSION['nom'])) {
 <body>
     <h1>Hello, <?php echo $_SESSION['nom']?></h1>
     <a href="logout.php">Logout</a>
-    
-</body>
-</html>
 
-<?php
+    <?php
 } else {
         header("Location: index.php");
         exit();
     }
 ?>
+
+    <?php
+    include "db_conn.php"; // Using database connection file here
+
+    $sql = "SELECT * FROM logement ";
+    
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        ?>
+
+
+    <table class="table table-dark table-striped">
+        <tr>
+            <th>Type de logement</th>
+            <th>Adultes</th>
+            <th>Animaux</th>
+            <th>Prix</th>
+            <th>Enfants</th>
+            <th>Réserver</th>
+
+
+        </tr>
+        <?php // output data of each row
+            ?>
+        <?php while ($row = $result->fetch_assoc()) {
+                ?> <tr>
+            <td> <?php echo $row["type"]; ?></td>
+            <td><?php echo $row["adult"]; ?> </td>
+            <td> <?php echo $row["pet"]; ?> </td>
+            <td><?php echo $row["price"]; ?> </td>
+            <td> <?php echo $row["child"]; ?> </td>
+            <td> <button><a href='book.php'>Réserver</a></button> </td>
+            
+        </tr>
+        <?php
+            } ?>
+    </table>
+
+
+    <?php
+    } else { ?>
+    0 results
+    <?php }
+
+    $conn->close();
+    ?>
+    
+</body>
+</html>
+
