eb71f5051574f14a09e1fb9924da6a165effb485
Nicolas Chalono <nicolas.chalono@gmail.com>
2021-11-26 14:24:10 +0100

    resolve conflicts on login page

--- listing/listing.php
+++ listing/listing.php
@@ -12,7 +12,7 @@ $faker = 'Faker\Factory'::create()
 ?><!DOCTYPE html>
 <html>
 <head> 
-  <title>Title</title>
+  <title>Nos logements disponibles</title>
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
         integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
         crossorigin=""/>
@@ -27,12 +27,18 @@ $faker = 'Faker\Factory'::create()
 <div class="container">
 
   <div class="list">
-	<?php
-        if (isset($_POST['submit-search'])) {
-            $district = mysqli_real_escape_string($conn, $_POST['address']);
-            $checkin = mysqli_real_escape_string($conn, $_POST['check-in']);
-            $checkout = mysqli_real_escape_string($conn, $_POST['check-out']);
-            $traveller = mysqli_real_escape_string($conn, $_POST['traveller']);
+	<?php  
+
+        if (isset($_GET['submit-search'])) {
+            session_start();
+
+           
+            $_SESSION['traveller'] =  $_GET['traveller'];
+            var_dump($_SESSION, $_GET);
+            $district = mysqli_real_escape_string($conn, $_GET['address']);
+            $checkin = mysqli_real_escape_string($conn, $_GET['check-in']);
+            $checkout = mysqli_real_escape_string($conn, $_GET['check-out']);
+            $traveller = mysqli_real_escape_string($conn, $_GET['traveller']);
 
             // die($district. " , " . $checkin . "," . $checkout . "," . $traveller);
 
@@ -47,24 +53,28 @@ $faker = 'Faker\Factory'::create()
             
 
             if ($queryResult > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                  $dateformat_in = explode('/', $checkin);
-                  $tab_in = $dateformat_in[2] . '-' . $dateformat_in[1] . '-' . $dateformat_in[0];
+              if (!empty($checkin)) {
+                  while ($row = mysqli_fetch_assoc($result)) {
+                      $dateformat_in = explode('/', $checkin);
+                      $tab_in = $dateformat_in[2] . '-' . $dateformat_in[1] . '-' . $dateformat_in[0];
 
-                  $dateformat_out = explode('/', $checkout);
-                  $tab_out = $dateformat_out[2] . '-' . $dateformat_out[1] . '-' . $dateformat_out[0];
+                      $dateformat_out = explode('/', $checkout);
+                      $tab_out = $dateformat_out[2] . '-' . $dateformat_out[1] . '-' . $dateformat_out[0];
 
-                  $sql_date= "SELECT booking.checkin, booking.checkout, booking.rental_id from booking 
-                  JOIN rental ON booking.rental_id = rental.id
-                  WHERE rental_id = " .$row['id']. " AND booking.checkin >= $tab_in AND booking.checkout <= $tab_out;
+                      $_SESSION['check-in'] =  $tab_in;
+                      $_SESSION['check-out'] =  $tab_out;
+                     
+                    $sql_date= "SELECT booking.checkin, booking.checkout, booking.rental_id from booking 
+                    JOIN rental ON booking.rental_id = rental.id
+                    WHERE rental_id = " .$row['id']. " AND  booking.checkin <= '$tab_in' AND booking.checkout >= '$tab_out';
                   
                   -- booking.checkin between '$tab_in' AND '$tab_out' AND 
                   -- booking.checkout between'$tab_in' AND '$tab_out'";
 
-                    $result2 = mysqli_query($conn, $sql_date);
-                    $queryResult2 = mysqli_num_rows($result2);
+                      $result2 = mysqli_query($conn, $sql_date);
+                      $queryResult2 = mysqli_num_rows($result2);
 
-                    if (empty($queryResult2)) { ?>
+                      if (empty($queryResult2)) { ?>
                     <div class="item js-marker" data-lat="<?= $faker->latitude(48.86325, 48.86852) ?>" data-lng="<?= $faker->longitude(2.26993, 2.40441) ?>" data-price="<?= $row["price"] ?>">
                       <a href="<?php echo "/rooms.php?id=" . $row["id"]; ?>"><img src="uploads/<?=$row['image_url']?>"></a>
                       <h4><?php echo $row["type"] . ' | ' . $row["price"] . ' € / nuit'; ?></h4>
@@ -73,6 +83,22 @@ $faker = 'Faker\Factory'::create()
                     </div>
                   <?php
                   }
+                  }
+              }
+            }
+
+            if ($queryResult > 0) {
+                if (empty($checkin)) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                  ?>
+                  <div class="item js-marker" data-lat="<?= $faker->latitude(48.86325, 48.86852) ?>" data-lng="<?= $faker->longitude(2.26993, 2.40441) ?>" data-price="<?= $row["price"] ?>">
+                      <a href="<?php echo "/rooms.php?id=" . $_GET[$row["id"]];  ?>"><img src="uploads/<?=$row['image_url']?>"></a>
+                      <h4><?php echo $row["type"] . ' | ' . $row["price"] . ' € / nuit'; ?></h4>
+                      <p><?php echo $row["description"] ?></p>
+                      <p><?php echo $row["district"] ?></p>
+                    </div>
+                  <?php
+                    }
                 }
             }
   ?>
