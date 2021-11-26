555be5e08180b972756e054ef06464ee759c65a1
Océane Raubitschek <oceane@MacBook-Pro-de-Oceane.local>
2021-11-26 17:41:23 +0100

    login_form_centered

--- form_login.php
+++ form_login.php
@@ -17,43 +17,51 @@
           <img src="/uploads/logo.png" alt="" />
         </a>
       </div>
-   </nav>
-  <!-- Ending navbar -->
-    <div class="login_form">
-      <form action="login.php" method="post" >
-          <div class="modal-dialog">
-        
-              <div class="modal-content">
-                  <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Se connecter</h5>
-                  <?php if (isset($_GET['error'])) { ?>
-                  <p class="error"><?php echo $_GET['error']; ?></p>
-                  <?php }
-                  ?>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                  <form>
-                      <div class="mb-3">
-                      <label for="login-name" class="col-form-label">Login:</label>
-                      <input type="text" class="form-control" id="login-name" name="lastname"/>
+    </nav>
+    <!-- Ending navbar -->
+    <div class="cover">
+      <div class="container-login content" >
+        <div class="login-form">
+          <form 
+            action="login.php" 
+            method="post" 
+            class="login_form"
+          >
+              <div class="modal-dialog">
+            
+                  <div class="modal-content">
+                      <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Se connecter</h5>
+                      <?php if (isset($_GET['error'])) { ?>
+                      <p class="error"><?php echo $_GET['error']; ?></p>
+                      <?php }
+                      ?>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                       </div>
-                      <div class="mb-3">
-                      <label for="message-text" class="col-form-label">Mot de passe:</label>
-                      <input type="password" class="form-control" id="login-name" name="password" />
+                      <div class="modal-body">
+                      <form>
+                          <div class="mb-3">
+                          <label for="login-name" class="col-form-label">Login:</label>
+                          <input type="text" class="form-control" id="login-name" name="lastname"/>
+                          </div>
+                          <div class="mb-3">
+                          <label for="message-text" class="col-form-label">Mot de passe:</label>
+                          <input type="password" class="form-control" id="login-name" name="password" />
+                          </div>
+                      </form>
                       </div>
-                  </form>
-                  </div>
-                  <div class="modal-footer">
-                  <button type="button submit" class="btn btn-primary">Connexion</button>
+                      <div class="modal-footer">
+                      <button type="button submit" class="btn btn-primary">Connexion</button>
+                      </div>
                   </div>
               </div>
-          </div>
-      </form>
+          </form>
+        </div>
+      </div>
     </div>
-  </main>  
+  </main> 
+
   <footer>
-  <hr>
   <div class="footer">
       
     <p><b> © 2021 DonkeyStay</b></p>
